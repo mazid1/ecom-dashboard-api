7606d413aaf2b5676b25a45ab8e0725dc9283b85
Mazedul Islam <mazidmailbox@gmail.com>
2023-06-07 17:00:41 +0600

    feat: schema validation for config module using joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3333),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
