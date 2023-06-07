a670708a33b86dc9959bf40aabb49f6619aeb2e2
Mazedul Islam <mazidmailbox@gmail.com>
2023-06-07 17:24:44 +0600

    feat: products module skeleton added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './modules/products/products.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -12,6 +13,7 @@ import * as Joi from 'joi';
         PORT: Joi.number().default(3333),
       }),
     }),
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
