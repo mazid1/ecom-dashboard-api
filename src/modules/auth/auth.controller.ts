62a4bb9433818688f4a2a418e9c6bf8554b41c07
Mazedul Islam <mazidmailbox@gmail.com>
2023-06-10 22:51:16 +0600

    refactor: unnecessary codes removed

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,17 +8,16 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { LocalAuthGuard } from './guards/local-auth.guard';
-import { RequestWithUser } from './types/request-with-user';
+import { ApiTags } from '@nestjs/swagger';
 import MongooseClassSerializerInterceptor from 'src/interceptors/mongoose-class-serializer.interceptor';
 import { User } from '../users/schemas/user.schema';
-import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { UsersService } from '../users/users.service';
-import JwtRefreshGuard from './guards/jwt-refresh.guard';
+import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto';
-import { Request } from 'express';
-import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import JwtRefreshGuard from './guards/jwt-refresh.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { RequestWithUser } from './types/request-with-user';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -30,25 +29,8 @@ export class AuthController {
 
   @UseInterceptors(MongooseClassSerializerInterceptor(User))
   @Post('register')
-  async register(
-    @Body() registrationData: RegisterDto,
-    @Req() request: Request,
-  ) {
+  async register(@Body() registrationData: RegisterDto) {
     const user = await this.authService.register(registrationData);
-
-    const accessTokenCookie = this.authService.getCookieWithAccessToken(
-      user._id,
-    );
-    const { cookie: refreshTokenCookie, token: refreshToken } =
-      this.authService.getCookieWithRefreshToken(user._id);
-
-    // store refresh token in DB
-    await this.usersService.setCurrentRefreshToken(refreshToken, user._id);
-
-    request.res.setHeader('Set-Cookie', [
-      accessTokenCookie,
-      refreshTokenCookie,
-    ]);
     return user;
   }
 
